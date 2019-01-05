b6df6250b78b0737d72d97a84ce8de52a9c9e21d
Thyerry <thyerry.nunes@gmail.com>
2019-01-05 17:43:09 -0300

    Edit main.pl

--- Planejador/main.pl
+++ Planejador/main.pl
@@ -2,13 +2,31 @@
 % voo(Cidade1, Cidade2, HorarioSaída, HorarioChegada, DiaDaSemana)
 
 % Axiomas
-voo(recife, salvador, 10, 13, [seg, qua, sex, dom]).
-voo(salvador, palmas, 15, 17, [seg, ter, qui, sab]).
+
+% Saídas de Recife
+voo(recife, salvador, 10.0, 11.2, [seg, qua, sex, dom]).
+voo(recife, belem, 5, 8, [seg, qua, sex, dom]).
+voo(recife, saoPaulo, 1, 4.09, [seg, qua, sex, dom]).
+voo(recife, portoAlegre, 10, 14.12, [seg, qua, sex, dom]).
+voo(recife, cuiaba, 10, 14.12, [seg, qua, sex, dom]).
+
+% Saídas de Salvador
+voo(salvador, palmas, 12.3, 17, [seg, ter, qui, sab]).
+
 
 % Predicados
+
+% Predicado base que é o de viagem, ainda não considera o horario de chegada nem o de saída.
 viagem(Saida, Destino):-
-    voo(Saida, Destino, _, _, _).
+    voo(Saida, Destino, _, _, _),
+    % Nesse write eu quero colocar o caminho inteiro traçado até chegar ao destino
+    % Do jeito que está, não funciona corretamente, isso será feito através de um predicado próprio.
+    write("Caminho tracado: "),  
+    write(Saida),
+    write(" -> "),
+    write(Destino).
 
 viagem(Saida, Destino):-
     voo(Saida, Escala, _, _, _),
     viagem(Escala, Destino).
+
